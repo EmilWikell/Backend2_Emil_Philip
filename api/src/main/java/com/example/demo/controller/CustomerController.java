4869d2c37b67adb1d4b0e0f0a37aab2d3d2cbd79
Philip Tonaczew <58476139+Tyareenbajka@users.noreply.github.com>
2022-05-12 10:39:33 +0200

    testing commit from api folder

--- api/src/main/java/com/example/demo/controller/CustomerController.java
+++ api/src/main/java/com/example/demo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.example.demo.repository.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+//Controller
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
